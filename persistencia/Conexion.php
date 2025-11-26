f6579fa63bb6b626d644bf0edc0404fb92613067
ThomasAguirre <tomasesteban20010@gmail.com>
2025-11-26 07:12:29 -0500

    Moficicacion final ojala

--- persistencia/Conexion.php
+++ persistencia/Conexion.php
@@ -4,25 +4,27 @@ class Conexion {
     private $conexion;
     private $resultado;
 
-    public function abrir() {
-        if ($_SERVER['REMOTE_ADDR'] == "::1") {
-            $this->conexion = new mysqli("localhost", "root", "", "aerolinea", 3306);
-        } else {
-     $this->conexion = new mysqli(
-    "localhost",
-    "itiud_aplint2",   // usuario
-    "9IGmG24ue&",      // clave
-    "itiud_aplint"     // <<-- la base de datos REAL
-);
+public function abrir() {
+    if ($_SERVER['REMOTE_ADDR'] == "::1") {
 
+        // --- local ---
+        $this->conexion = new mysqli("localhost", "root", "", "aerolinea", 3306);
 
+    } else {
 
-        }
-
-        if ($this->conexion->connect_errno) {
-            die("Error de conexión: " . $this->conexion->connect_error);
-        }
+        // --- PRODUCCIÓN ---
+        $this->conexion = new mysqli(
+            "usXXX.cloudlogin.co",    // <-- CAMBIA ESTO POR EL HOST REAL DEL PMA
+            "itiud_aplint2",
+            "9IGmG24ue&",
+            "itiud_aplint"
+        );
     }
+
+    if ($this->conexion->connect_errno) {
+        die("Error de conexión: " . $this->conexion->connect_error);
+    }
+}
 
     public function cerrar() {
         if ($this->conexion) {
